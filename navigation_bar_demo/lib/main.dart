8e8a1665cbb088754ac361b132fdb8cc548c9737
ok <kelvin147789@gmail.com>
2019-03-25 20:48:55 +0800

    add navigation bar

---
+++ navigation_bar_demo/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:layout_demo_flutter/main_page.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: MainPage(),
+    );
+  }
+}
