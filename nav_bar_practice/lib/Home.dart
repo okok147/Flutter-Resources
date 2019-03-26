7c5a2a677888e925ec084c9da3a2971d7d35d024
ok <kelvin147789@gmail.com>
2019-03-26 14:58:18 +0800

    update

--- nav_bar_practice/lib/Home.dart
+++ nav_bar_practice/lib/Home.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(HomePage());
 
 
 
 class HomePage extends StatelessWidget {
+  // This widget is the root of your application.
   @override
-  Widget build (BuildContext context) {
-    return Container (
-      padding: EdgeInsets.all(25.0),
-      child: Text('Home Page111',style: TextStyle(fontSize: 36.0),),
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Code Sample for material.Scaffold',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+
+
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _count = 0;
+
+
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+
+      body: Center(
+        child: Text('Welcome.'),
+      ),
+
+
+
     );
   }
 }
--- nav_bar_practice/lib/main.dart
+++ nav_bar_practice/lib/main.dart
@@ -6,54 +6,123 @@ import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 void main() => runApp(MyApp());
 
+
 class MyApp extends StatefulWidget{
+
+
 
 
   @override
   State<StatefulWidget> createState(){
     return MyAppState();
+
+
   }
 }
 
 class MyAppState extends State<MyApp> {
 
+
+  //This is important,to set every page for a value.
+
   int _selectPage = 0;
+
+
+  //depends on _selectPage int,then set _pageOption in body ,example _pageOption  (which put index = _selectpage = 1)
   final _pageOption = [
+    //This presents 1
     HomePage(),
-    WorkPage(),
+    //This presents 2
     LandscapePage(),
+    //This presents 3
+    WorkPage(),
+
   ];
 
 
   @override
   Widget build(BuildContext context) {
+
+    //This is the whole app
     return MaterialApp(
       title: 'Flutter Demo',
+
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+
       ),
       home: Scaffold(
-        appBar: AppBar(title: Text('Bottom Nav Bar Video'),),
+
+        //There are three parts of app : appBar (Top),  body (Middle), bottomNavigationBar (Bottom)
+
+        //Part 1
+
+        appBar:
+
+        PreferredSize(
+        preferredSize: Size.fromHeight(0.0),
+          child: AppBar(title: Text('Curved Nav Bar'))
+
+
+
+
+
+
+
+
+
+
+
+        ),
+
+
+        //Part 2
+        //***This is the code makes array works as page changing method,it sets page as t
         body: _pageOption[_selectPage],
-        bottomNavigationBar: CurvedNavigationBar(
+
+        //Part 3
+        bottomNavigationBar:
+
+        PreferredSize(
+        preferredSize: Size.fromHeight(40.0),
+          child:
+
+
+
+        CurvedNavigationBar(
+
+
+
+
+
 
 
           //each index represent a number,when tap,var a number then switch to that page.
           onTap: (int index) {
             setState(() {
+
+              //set _selectpage as index,which is items array int
               _selectPage = index;
             });
           },
 
 
-
+          //It is a array,icon 1 = 1 = index,refers to onTap setState
           items: <Widget>[
-            Icon(Icons.add, size: 30),
-            Icon(Icons.list, size: 30),
-            Icon(Icons.compare_arrows, size: 30),
+            Icon(Icons.home, size: 25),
+            Icon(Icons.search, size: 25),
+            Icon(Icons.person, size: 25),
+
+
+
+
           ],
+          //animation
+          animationDuration: Duration(milliseconds: 600),
+          animationCurve: Curves.easeInOut,
+        ),)
+
+
         ),
-      ),
     );
   }
 
