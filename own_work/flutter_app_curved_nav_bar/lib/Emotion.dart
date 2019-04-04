14ffef8e29e5e6eefe4b5a6915fdaed60de61c1b
ok <kelvin147789@gmail.com>
2019-04-04 19:12:48 +0800

    animation with button

--- own_work/flutter_app_curved_nav_bar/lib/Emotion.dart
+++ own_work/flutter_app_curved_nav_bar/lib/Emotion.dart
@@ -1,46 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:smart_flare/smart_flare.dart';
+import 'package:flutter_app_curved_nav_bar/Homepage.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter_app_curved_nav_bar/Emotion.dart';
+import 'package:flutter_app_curved_nav_bar/Profile.dart';
 
-import 'package:flare_flutter/flare_actor.dart';
+void main() {
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: new Emotion(),
 
-void main() => runApp(Emotion());
+    //direct to another routes
+    routes: <String,WidgetBuilder>{
+      "/BottomNavBar" :(BuildContext context) => new Emotion(),
 
+    },
 
-class Emotion extends StatelessWidget {
+  ));
+}
 
+class Emotion extends StatefulWidget {
   @override
-  Widget build(BuildContext context){
-    return new MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Emotion',
-      darkTheme: ThemeData.dark(
+  _FlareDemoState createState() => _FlareDemoState();
+}
 
-      ),
+class _FlareDemoState extends State<Emotion> {
+  @override
+  Widget build(BuildContext context) {
+    var animationWidth = 185.0;
+    var animationHeight = 140.0;
+    var thirdOfWidth = animationWidth / 3;
+    var activeAreas = [
+      // Insert top-left
 
-      home: MyStatefulWidget(),
-    );
+      // Insert middle
+      ActiveArea(
+        // debugArea: true,
+          debugArea: false,
+
+          area: Rect.fromLTWH(thirdOfWidth*1.4, 212.0, animationWidth / 1.45 , animationHeight / 2),
+          guardComingFrom: ['Activate'],
+
+          animationsToCycle: ['Off','On'],
+          onAreaTapped: () {
+
+            //New method for direct to another page
+
+          }),
+      // Insert top-right
+
+      // Insert bottom half area
+      //it demonstrates how to define the action of flr
+
+    ];
+
+    return Scaffold(
+
+
+        backgroundColor: Color.fromARGB(255, 255, 255, 255),
+        body: Align(
+
+            alignment: Alignment.center,
+            child: SmartFlareActor(
+              width: 300.0,
+              height: 500.0,
+              filename: 'assets/emotion.flr',
+
+
+              activeAreas: activeAreas,
+
+
+            )));
   }
 }
-
-
-class MyStatefulWidget extends StatefulWidget {
-
-  MyStatefulWidget ({Key key}) : super (key:key);
-
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-}
-
-
-
-class _MyStatefulWidgetState extends State <MyStatefulWidget> {
-
-  Widget build (BuildContext context) {
-
-    return new FlareActor("assets/see.flr",
-      alignment:Alignment.center, fit:BoxFit.contain, animation:"full_loop",);
-
-  }
-}
-
-
 
--- own_work/flutter_app_curved_nav_bar/lib/Profile.dart
+++ own_work/flutter_app_curved_nav_bar/lib/Profile.dart
@@ -1,40 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:smart_flare/smart_flare.dart';
+import 'package:flutter_app_curved_nav_bar/Homepage.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter_app_curved_nav_bar/Emotion.dart';
+import 'package:flutter_app_curved_nav_bar/Profile.dart';
 
-void main() => runApp(Profile());
+void main() {
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: new Profile(),
 
+    //direct to another routes
+    routes: <String,WidgetBuilder>{
+      "/BottomNavBar" :(BuildContext context) => new Profile(),
 
-class Profile extends StatelessWidget {
+    },
 
+  ));
+}
+
+class Profile extends StatefulWidget {
   @override
-  Widget build(BuildContext context){
-    return new MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Profile',
-      darkTheme: ThemeData.dark(
+  _FlareDemoState createState() => _FlareDemoState();
+}
 
-      ),
+class _FlareDemoState extends State<Profile> {
+  @override
+  Widget build(BuildContext context) {
+    var animationWidth = 185.0;
+    var animationHeight = 140.0;
+    var thirdOfWidth = animationWidth / 3;
+    var activeAreas = [
+      // Insert top-left
 
-      home: MyStatefulWidget(),
-    );
+      // Insert middle
+      ActiveArea(
+        // debugArea: true,
+          debugArea: false,
+
+          area: Rect.fromLTWH(thirdOfWidth*0.28, 212.0, animationWidth * 2  , animationHeight * 1),
+          guardComingFrom: ['Activate'],
+
+
+          animationsToCycle: ['human_1','human_2','human_3','human_4','human_5'],
+          onAreaTapped: () {
+
+
+
+            //New method for direct to another page
+
+          }),
+      // Insert top-right
+
+      // Insert bottom half area
+      //it demonstrates how to define the action of flr
+
+    ];
+
+    return Scaffold(
+
+
+        backgroundColor: Color.fromARGB(255, 255, 255, 255),
+        body: Align(
+
+            alignment: Alignment.center,
+            child: SmartFlareActor(
+              width: 300.0,
+              height: 500.0,
+              filename: 'assets/human_revolution.flr',
+              startingAnimation: 'human_1',
+
+
+
+              activeAreas: activeAreas,
+
+
+            )));
   }
 }
 
-
-class MyStatefulWidget extends StatefulWidget {
-  MyStatefulWidget ({Key key}) : super (key:key);
-
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-}
-
-
-
-class _MyStatefulWidgetState extends State <MyStatefulWidget> {
-  Widget build (BuildContext context) {
-    return Scaffold (
-      body: Center(
-        child: Text('Edit your information'),
-      ),
-    );
-  }
-}
