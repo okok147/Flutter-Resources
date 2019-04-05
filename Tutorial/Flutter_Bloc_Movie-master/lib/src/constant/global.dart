3cc6a912d9f0e06eec48a4053cac8de644aed532
ok <kelvin147789@gmail.com>
2019-04-05 14:00:33 +0800

    focus

--- Tutorial/Flutter_Bloc_Movie-master/lib/src/constant/global.dart
+++
@@ -1,7 +0,0 @@
-class MovieListType{
-
-  static String popular ="popular";
-  static String nowPlaying ="now_playing";
-  static String topRated ="top_rated";
-  static String upcoming ="upcoming";
-}
--- own_work/smart_flare-master/example/README.md
+++
@@ -1,3 +0,0 @@
-# Smarter Flare animation using Flutter - Final
-
-This is the final result for the tutorial. [Smarter Flare animations with Flutter]()
