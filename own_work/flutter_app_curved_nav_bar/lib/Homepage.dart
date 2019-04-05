74e3b0addf7e27432c694052e4d2b90828fcd50d
ok <kelvin147789@gmail.com>
2019-04-05 14:06:27 +0800

    change

--- own_work/flutter_app_curved_nav_bar/lib/Homepage.dart
+++ own_work/flutter_app_curved_nav_bar/lib/Homepage.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smart_flare/smart_flare.dart';
-import 'package:flutter_app_curved_nav_bar/Homepage.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-import 'package:flutter_app_curved_nav_bar/Emotion.dart';
-import 'package:flutter_app_curved_nav_bar/Profile.dart';
+
 
 void main() {
   runApp(new MaterialApp(
@@ -66,6 +62,8 @@ class _FlareDemoState extends State<Homepage> {
 
               filename: 'assets/home.flr',
               startingAnimation: 'Sun Rotate' ,
+              height: 100.0,
+              width: 100.0,
 
 
 
