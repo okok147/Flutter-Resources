96e57615e50722a8e7ef55ecff62b5d80b19986d
ok <kelvin147789@gmail.com>
2019-04-06 19:16:19 +0800

    Update theme.dart

--- Learning Example/TheGorgeousLogin-master/lib/style/theme.dart
+++ Learning Example/TheGorgeousLogin-master/lib/style/theme.dart
@@ -6,8 +6,8 @@ class Colors {
 
   const Colors();
 
-  static const Color loginGradientStart = const Color(0xFFfbab66);
-  static const Color loginGradientEnd = const Color(0xFFf7418c);
+  static const Color loginGradientStart = const Color(0xFFf3f0ef);
+  static const Color loginGradientEnd = const Color(0xFF7d42fef   );
 
   static const primaryGradient = const LinearGradient(
     colors: const [loginGradientStart, loginGradientEnd],
