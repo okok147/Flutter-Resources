bd20ad6e8d2084efec0e992e2ea33084e509fb8d
Lau Ka Ho <kaho@Laus-MacBook-Pro.local>
2019-04-12 16:26:26 +0800

    update

--- Learning Example/flutter_wallet_ui-master/lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_wallet_ui_challenge/src/app.dart';
-
-void main() => runApp(App());
-
-
-
-
--- Learning Example/flutter_wallet_ui-master/lib/src/utils/screen_size.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-const double baseHeight = 650.0;
-
-double screenAwareSize(double size, BuildContext context) {
-  return size * MediaQuery.of(context).size.height / baseHeight;
-}
