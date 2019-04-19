c986a207ffecf9d5efb57813938dcd4ab236b408
ok <kelvin147789@gmail.com>
2019-04-19 18:34:25 +0800

    dashboard
    decoding,unfinished.

--- Own_Work/clone_travel_ui_02/lib/ui/dashboard.dart
+++ Own_Work/clone_travel_ui_02/lib/ui/dashboard.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:clone_travel_ui/ui/components/bottomnavbar.dart';
+import 'package:clone_travel_ui/ui/dashboard/explore_page.dart';
+import 'package:clone_travel_ui/ui/dashboard/home_page.dart';
+import 'package:clone_travel_ui/ui/dashboard/profile_page.dart';
+import 'package:clone_travel_ui/ui/dashboard/tours_page.dart';
+import 'package:clone_travel_ui/ui/tabnavigations/homenav.dart';
 
 
 class DashboardPage extends StatefulWidget {
@@ -7,8 +13,6 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage> {
-
-
   static final Map<int, GlobalKey<NavigatorState>> navigatorKeys = {
     0: GlobalKey<NavigatorState>(),
     1: GlobalKey<NavigatorState>(),
@@ -16,10 +20,14 @@ class _DashboardPageState extends State<DashboardPage> {
     3: GlobalKey<NavigatorState>(),
   };
 
-
   int _selected = 0;
 
   List<Widget> _children = [
+    HomeTabNavigator(navigatorKey : navigatorKeys[0],),
+    //ToursPage(),
+    //ExplorePage(),
+    //ProfilePage(),
+
 
   ];
 
@@ -29,16 +37,30 @@ class _DashboardPageState extends State<DashboardPage> {
     });
   }
 
+
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-
-      onWillPop: () async =>
+    // TODO: implement build
+    return WillPopScope (
+      onWillPop:  () async =>
       !await navigatorKeys[_selected].currentState.maybePop(),
+
       child: Scaffold(
         body: _children[_selected],
+        bottomNavigationBar: FABBottomAppBar(
+          color: Colors.grey,
+          selectedColor: Colors.blueAccent,
+          onTabSelected: _selectedTab,
+          items: [
+            BottomAppBarItem(iconData: Icons.add_to_home_screen,text: "Home"),
+            BottomAppBarItem(iconData: Icons.beach_access,text: 'Tours'),
+            BottomAppBarItem(iconData: Icons.location_searching,text: 'Explore'),
+            BottomAppBarItem(iconData: Icons.person_outline,text: 'Profile'),
 
+          ],
+        ),
       ),
+
     );
   }
 }
