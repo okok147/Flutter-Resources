6c518a0afd160275eb23052ff17fc4cfd0b7f079
ok <kelvin147789@gmail.com>
2019-04-20 16:41:14 +0800

    Update main.dart

--- Own_Work/flutter_app_intro_slider/lib/main.dart
+++ Own_Work/flutter_app_intro_slider/lib/main.dart
@@ -6,11 +6,16 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
 
 
+
   // This widget is the root of your application.
   @override
+
   Widget build(BuildContext context) {
+
+
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
 
         // This is the theme of your application.
@@ -30,6 +35,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
+
   MyHomePage({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
