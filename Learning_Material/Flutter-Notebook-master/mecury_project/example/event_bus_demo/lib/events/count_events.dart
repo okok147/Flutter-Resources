05e8dfb627e556b50e7f6ec24e0e1329367c7a63
Lau Ka Ho <kaho@Laus-MacBook-Pro.local>
2019-05-01 22:33:13 +0800

    remove example of notebook

--- Learning_Material/Flutter-Notebook-master/mecury_project/example/event_bus_demo/lib/events/count_events.dart
+++
@@ -1,5 +0,0 @@
-class CountEvent {
-  final int count;
-
-  CountEvent(this.count);
-}
--- Learning_Material/Flutter-Notebook-master/mecury_project/example/event_bus_demo/lib/tools/bus.dart
+++
@@ -1,4 +0,0 @@
-import 'package:event_bus/event_bus.dart';
-import 'package:rxdart/rxdart.dart';
-
-EventBus behaviorBus = EventBus.customController(BehaviorSubject());
