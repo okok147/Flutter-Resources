9042334cac22d2028a5733f33b7dacfdbec047c0
ok <kelvin147789@gmail.com>
2019-05-06 16:05:59 +0800

    Udacity flutter tutorial 11 dissect hello rectangle

--- Own_Work/flutter_rectangle/lib/main.dart
+++ Own_Work/flutter_rectangle/lib/main.dart
@@ -1,50 +1,34 @@
 import 'package:flutter/material.dart';
 
-void main() {
-
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: '???',
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Hello AppBar'),
-      ),
-      body: HelloRectangle(),
-    ),
-
-  ));
-
-}
-
-class HelloRectangle extends StatelessWidget {
+class HelloRectangele extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Center (
-
-      child:Container(
-      color: Colors.greenAccent,
-      height: 400.0,
-      width: 300.0,
-
-
-
-
-
-      child: Center(
-        child: Text(
-          'YO',
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 100.0,fontFamily: 'Montserrat'),
-
+    return Center(
+      child: Container(
+        color: Colors.greenAccent,
+        height: 400.0,
+        width: 300.0,
+        child: Center(
+          child: Text('Hello',style: TextStyle(fontSize: 100.0),
+          ),
         ),
-
-
       ),
-
-
-    ),
-
     );
   }
+
+
+}
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('HAHA'),
+      ),
+      body: HelloRectangele(),
+    ),
+  ),
+  );
 }
