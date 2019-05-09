ee14404391e24fb07bb58714bb55446d42c07589
ok <kelvin147789@gmail.com>
2019-05-09 22:58:31 +0800

    add sketch UI

--- Own_Work/flutter_rectangle/lib/main.dart
+++ Own_Work/flutter_rectangle/lib/main.dart
@@ -1,34 +1,38 @@
 import 'package:flutter/material.dart';
 
-class HelloRectangele extends StatelessWidget{
+class HelloRectangle extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Center(
-      child: Container(
-        color: Colors.greenAccent,
-        height: 400.0,
-        width: 300.0,
-        child: Center(
-          child: Text('Hello',style: TextStyle(fontSize: 100.0),
+      child: Padding(
+        padding: const EdgeInsets.all(200.0),
+        child: Container(
+          color: Colors.lightBlueAccent,
+          height: 400.0,
+          width: 300.0,
+          child:  Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Center(child: Text('HELLO',style: TextStyle(fontSize: 40.0))),
           ),
+
+
         ),
       ),
+
     );
   }
-
-
 }
 
+
+
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('HAHA'),
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: HelloRectangle(),
       ),
-      body: HelloRectangele(),
     ),
-  ),
   );
 }
