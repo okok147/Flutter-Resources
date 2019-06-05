1de12a4a74df377ddce0b6b600fe5a9fcbab84c3
ok <kelvin147789@gmail.com>
2019-06-05 14:22:05 +0800

    update

--- Work/layout/lib/main.dart
+++ Work/layout/lib/main.dart
@@ -1,36 +1,14 @@
 import 'package:flutter/material.dart';
+
 void main() => runApp(MaterialApp(
-
-
   home: MyApp(),
   debugShowCheckedModeBanner: false,
-
 ));
+
 
 class MyApp extends StatefulWidget{
   @override
   _MyAppState createState() => new _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-
-      backgroundColor: Color(0xFFFFFFFF),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            IconButton(
-              icon: Icon(
-                
-              ),
-            )
-          ],
-        ),
-      ),
-
-    );
-  }
-}
+class _MyAppState extends State<MyApp>
