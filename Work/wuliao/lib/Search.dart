9c038e83894acc56182d52dfcf810e96409deec2
kaho <kaho@kahos-iMac.local>
2019-08-21 15:09:22 +0800

    update

--- Work/wuliao/lib/Search.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class SearchPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Container(color: Colors.redAccent,);
-  }
-}
