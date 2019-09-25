bbea1158a12c3abf48779c505fedc911af3188f4
Lau Ka Ho <kaho@Laus-MacBook-Pro.local>
2019-09-25 10:42:49 +0800

    Update flutter_export_environment.sh

--- Example/Best-Flutter-UI-Templates-master/best_flutter_ui_templates/ios/Flutter/flutter_export_environment.sh
+++ Example/Best-Flutter-UI-Templates-master/best_flutter_ui_templates/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/kaho/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/kaho/Documents/GitHub/flutter-development/Example/Best-Flutter-UI-Templates-master/best_flutter_ui_templates"
-export "FLUTTER_TARGET=/Users/kaho/Documents/GitHub/flutter-development/Example/Best-Flutter-UI-Templates-master/best_flutter_ui_templates/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/kaho/Documents/GitHub/flutter-ui-resources/Example/Best-Flutter-UI-Templates-master/best_flutter_ui_templates"
+export "FLUTTER_TARGET=/Users/kaho/Documents/GitHub/flutter-ui-resources/Example/Best-Flutter-UI-Templates-master/best_flutter_ui_templates/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/kaho/Documents/flutter/bin/cache/artifacts/engine/ios"
