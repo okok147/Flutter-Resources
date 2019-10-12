047f45c3e0fcfe40374c510ceec06c19edc0f638
kaho <kaho@kahos-iMac.local>
2019-10-12 13:04:26 +0800

    kyu6_bouncing_balls

--- Codewar/Python/bouncing_balls.py
+++ Codewar/Python/bouncing_balls.py
@@ -19,3 +19,29 @@ def bouncingBall(h,bounce,window):
 
     "***************************************************"
 
+def bouncingBall(start, p, height):
+    '''
+    start, height: positive int or float;
+    p, postive ratio(0~ 1)
+    '''
+    if  p == 1:
+        return -1
+
+    curr_height = start
+    times = 0
+    while curr_height > height:
+        if p*curr_height > height:
+            times += 2
+            curr_height *= p
+        else:
+            times += 1
+            curr_height *= p
+    return times
+
+
+print [30 * (0.66)**i for i in range(10)]
+print bouncingBall(3, 0.66, 1.5)
+
+print bouncingBall(30, 0.66, 1.5)
+
+print 30 * (0.66)**8
