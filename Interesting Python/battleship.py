f6fb97ec1f52f2a2045d55bfbedf20ef94764e1e
Lau Ka Ho <kaho@Laus-MacBook-Pro.local>
2019-10-23 10:22:17 +0800

    update

--- Interesting Python/battleship.py
+++ Interesting Python/battleship.py
@@ -1,28 +1,25 @@
-
-
-
 from random import randint
 
 board = []
 
 for x in range(0, 5):
-  board.append(["O"] * 5)
+    board.append(["O"] * 5)
 
 
 def print_board(board):
-  for row in board:
-    print " ".join(row)
+    for row in board:
+        print " ".join(row)
 
 
 print_board(board)
 
 
 def random_row(board):
-  return randint(0, len(board) - 1)
+    return randint(0, len(board) - 1)
 
 
 def random_col(board):
-  return randint(0, len(board[0]) - 1)
+    return randint(0, len(board[0]) - 1)
 
 
 ship_row = random_row(board)
@@ -35,8 +32,12 @@ guess_col = int(raw_input("Guess Col: "))
 
 # Write your code below!
 if guess_row == int(ship_row) and guess_col == int(ship_col):
-  print 'Congratulations! You sank my battleship!'
+    print 'Congratulations! You sank my battleship!'
 else:
-  print 'You missed my battleship!'
-  board[guess_row][guess_col] = "X"
-  print_board(board)
+    if guess_row not in range(5) or guess_col \
+            not in range(5):
+        print "Oops, that's not even in the ocean."
+    else:
+        print 'You missed my battleship!'
+        board[guess_row][guess_col] = "X"
+        print_board(board)
