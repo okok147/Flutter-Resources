bd606fd76c044c371ada596018fe8548ff49c8a2
OK <kelvin147789@gmail.com>
2019-10-24 21:00:05 +0800

    update

--- Python/Codewar/6kyu/Mexican Wave_6kyu.py
+++
@@ -1,7 +0,0 @@
-def wave(str):
-    return [str[:i] + str[i].upper() + str[i+1:] 
-    for i in range(len(str)) if str[i].isalpha()]
-
-
-
-#for examplefeatures = tf.layers.batch_normalization(features)
