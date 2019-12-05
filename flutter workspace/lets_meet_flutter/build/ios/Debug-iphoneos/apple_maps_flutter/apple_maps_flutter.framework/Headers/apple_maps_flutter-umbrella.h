e5b4aa9afd33a59b4538f85bb03d6064e61c72e3
kelvin lau <kelvin@ibasezero.com>
2019-12-05 15:01:38 +0800

    update

---
+++ flutter workspace/lets_meet_flutter/build/ios/Debug-iphoneos/apple_maps_flutter/apple_maps_flutter.framework/Headers/apple_maps_flutter-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "AppleMapsFlutterPlugin.h"
+
+FOUNDATION_EXPORT double apple_maps_flutterVersionNumber;
+FOUNDATION_EXPORT const unsigned char apple_maps_flutterVersionString[];
+
