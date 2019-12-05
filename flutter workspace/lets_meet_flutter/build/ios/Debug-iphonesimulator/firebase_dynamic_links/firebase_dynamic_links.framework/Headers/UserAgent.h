deac2fc165bdc6320a7195d0573305e2c18109fe
OK <kelvin147789@gmail.com>
2019-12-05 21:25:43 +0800

    add

---
+++ flutter workspace/lets_meet_flutter/build/ios/Debug-iphonesimulator/firebase_dynamic_links/firebase_dynamic_links.framework/Headers/UserAgent.h
@@ -0,0 +1,3 @@
+// Generated file, do not edit
+#define LIBRARY_VERSION @"0.5.0-8"
+#define LIBRARY_NAME @"flutter-fire-dl"
---
+++ flutter workspace/lets_meet_flutter/build/ios/Debug-iphonesimulator/flutter_facebook_login/flutter_facebook_login.framework/Headers/flutter_facebook_login-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "FacebookLoginPlugin.h"
+
+FOUNDATION_EXPORT double flutter_facebook_loginVersionNumber;
+FOUNDATION_EXPORT const unsigned char flutter_facebook_loginVersionString[];
+
