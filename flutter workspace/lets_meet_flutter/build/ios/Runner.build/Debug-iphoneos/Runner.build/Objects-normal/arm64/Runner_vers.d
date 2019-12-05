f355fd7d4fbe346bf709fa192b1b6b426dfd22cf
kelvin lau <kelvin@ibasezero.com>
2019-12-05 17:21:56 +0800

    busdriver

---
+++ flutter workspace/lets_meet_flutter/build/ios/Runner.build/Debug-iphoneos/Runner.build/Objects-normal/arm64/Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/kelvin/Documents/workspace/Flutter-Resources/flutter\ workspace/lets_meet_flutter/build/ios/Runner.build/Debug-iphoneos/Runner.build/DerivedSources/Runner_vers.c
